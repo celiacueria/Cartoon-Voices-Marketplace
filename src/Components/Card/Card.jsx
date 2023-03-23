8a9bbf1615c656c8d427679647e9f2e2d8d69647
pablo perez gonzalez <pabloperezgon91@gmail.com>
2023-03-23 12:34:50 +0100

    change img to video

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,26 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Card.css'
+import { MdClose } from "react-icons/md";
+
+
+import videomp4 from "./video.mp4"
+const imgurl = "https://i.pravatar.cc/150?u=pablo";
 
 
 function Card() {
+    let [img, setImg] = useState({ display: "block" })
+    let [video, setVideo] = useState({ display: "none" })
+
+    function changeImgVideo() {
+        setImg({ display: "none" })
+        setVideo({ display: "block" })
+    }
+
+    function resetImgVideo() {
+        setImg({display:"block"})
+        setVideo({display:"none"})
+    }
+
+
+
     return (
         <div class="card"  >
-            <div className='card-img'>
-                <img src="https://i.pravatar.cc/150?u=card" alt="ejemplo" />
+            <div class="card-img">
+                <img src={imgurl} onClick={changeImgVideo} style={img} alt="ejemplo" />
+                <MdClose  onClick={resetImgVideo} style={video}/>
+                <video src={videomp4} style={video} autoPlay controls />
             </div>
-            
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
+                <h5 class="card-title">Nombre</h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             </div>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">An item</li>
-                <li class="list-group-item">A second item</li>
-                <li class="list-group-item">A third item</li>
+            <ul class="card-body-ul">
+                <li class="list-group-item">categoria</li>
+                <li class="list-group-item">Email de contacto</li>
+                <li class="list-group-item">Precio</li>
             </ul>
-            <div class="card-body">
-                <a href=" " class="card-link">Card link</a>
-                <a href=" " class="card-link">Another link</a>
+            <div class="card-body-button">
+                <button type="button" class="btn btn-secondary btn-sm m-1">Contratar</button>
             </div>
         </div>
     )
