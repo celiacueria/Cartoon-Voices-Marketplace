f55d82c2cd7003f937f3e81f65df07003b3ff77d
Raul Villar <raultheviking@gmail.com>
2023-03-29 12:23:07 +0200

    Fixed page cards

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -2,40 +2,26 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import './Card.css'
 
+
 function Card(props) {
-    
-    let [img, setImg] = useState({ display: "block" })
-    let [video, setVideo] = useState({ display: "none" })
+
     const id = props.id;
     const editUrl = `/edit/${id}`;
-    
-    function changeImgVideo(event) {
-        if (event.type === "mouseover") {
-            setImg({ display: "none" })
-            setVideo({ display: "block" })
-        } else {
-            setImg({ dislay: "block" })
-            setVideo({ display: "none" })
-        }
-    }
 
     return (
-        <div class="card " style={{ width: "18rem" }} >
+        <div class="card " style={{ width: "18rem", height: "50vh" }} >
             <div class="card-img ">
-                <img src={props.img} onMouseOver={changeImgVideo} style={img} alt={props.name} />
-                <video src={props.video} onMouseOut={changeImgVideo} style={video} autoPlay controls />
-            </div>
-            <div class="card-body">
-                <h5 class="card-title">{props.name}</h5>
+                <img src={props.img} alt={props.name} width="200vh" height="200vh" />
             </div>
             <ul class="card-body-ul">
+                <h5 class="card-title">{props.name}</h5>
                 <li class="list-group-item">{props.category}</li>
                 <li class="list-group-item">{props.price}</li>
                 <li class="list-group-item">{props.email}</li>
             </ul>
             <div class="card-body-button">
                 <Link to="/contact">
-                <button type="button" class="btn btn-secondary btn-sm m-1">Contratar</button>
+                    <button type="button" class="btn btn-secondary btn-sm m-1">Contratar</button>
                 </Link>
                 <Link to={editUrl}>
                     <button type="button" class="btn btn-secondary btn-sm m-1">Editar</button>
